b4270b766351ea3ac3d3c1b217505c3a66afc17c
bugcloud <bugcloudisco@bugcloud.com>
2011-04-22 15:48:00 +0900

    トップページでMono.allを表示するようにコントローラを修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,6 @@
 class TopController < ApplicationController
   def index
+    @monos = Mono.all.order_by(:created_at.desc)
   end
 
 end
--- spec/controllers/top_controller_spec.rb
+++ spec/controllers/top_controller_spec.rb
@@ -1,10 +1,36 @@
 require 'spec_helper'
 
 describe TopController do
+  before(:all) do
+    Mono.delete_all
+  end
+
+  before(:each) do
+    @mono_carrot = Factory.build(:mono001)
+    @mono_carrot.save!
+    @mono_pine = Factory.build(:mono002)
+    @mono_pine.save!
+    @mono_juice = Factory.build(:mono003)
+    @mono_juice.save!
+  end
+
+  after(:each) do
+    Mono.delete_all
+    @mono_carrot = nil
+    @mono_pine = nil
+    @mono_juice = nil
+  end
 
   describe "GET 'index'" do
-    it "should be successful" do
+    it "should gets all Mono" do
+      monos = Mono.all
+      Mono.should_receive(:all).and_return(monos)
+      monos.should_receive(:order_by).with(:created_at.desc).and_return([@mono_carrot, @mono_pine, @mono_juice])
       get 'index'
+      assigns[:monos].size.should == 3
+      assigns[:monos].include?(@mono_carrot).should == true
+      assigns[:monos].include?(@mono_pine).should == true
+      assigns[:monos].include?(@mono_juice).should == true
       response.should be_success
     end
   end
