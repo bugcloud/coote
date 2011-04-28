0993455fc9195f1d0a088b5c13830e8663cada0a
bugcloud <bugcloudisco@bugcloud.com>
2011-04-28 18:36:58 +0900

    will_paginate実装

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def index
-    @monos = Mono.all.order_by(:created_at.desc)
+    @monos = Mono.order_by(:created_at.desc).paginate(:page => params[:page], :per_page => 5)
   end
 
 end
